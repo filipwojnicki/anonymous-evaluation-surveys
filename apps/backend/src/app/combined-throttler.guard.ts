47aa9136dba034fb499f72110877b60f98cf9103
Filip Wojnicki <filip.wojnicki@op.pl>
2025-02-13 14:37:16 +0100

    refactor(throttling): simplify request handling in CombinedThrottlerGuard

--- apps/backend/src/app/combined-throttler.guard.ts
+++ apps/backend/src/app/combined-throttler.guard.ts
@@ -5,13 +5,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 @Injectable()
 export class CombinedThrottlerGuard extends ThrottlerGuard {
   override getRequestResponse(context: ExecutionContext) {
-    if (context.getType() === 'http') {
-      return {
-        req: context.switchToHttp().getRequest(),
-        res: context.switchToHttp().getResponse(),
-      };
-    }
-
     const gqlCtx = GqlExecutionContext.create(context);
     const ctx = gqlCtx.getContext();
     return { req: ctx.req, res: ctx.res };
